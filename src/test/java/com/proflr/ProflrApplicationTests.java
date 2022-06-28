4879a5ab8e92ec1f15f005ca9e6df0181705497e
MrWanted <wanted.lepota@gmail.com>
2022-06-28 18:59:39 +0200

    initial commit

---
+++ src/test/java/com/proflr/ProflrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proflr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProflrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
